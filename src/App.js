a226edb3799cb573847330079e14cb98f505d5d2
Vladislav Akulinin <akulllvlllad@gmail.com>
2022-09-23 12:37:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
+import axios from 'axios'
+import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [fileData, setFileData] = React.useState(null)
+	
+
+
+	console.log(fileData)
+
+	const onSubmitHandler = e => {
+		e.preventDefault()
+		const formData = new FormData()
+		const file = e.target.files
+		console.log(file)
+		for (let i = 0; i < file.length; i++) {
+			formData.append('images', file[i])
+		}
+		
+		fetch('http://localhost:5000/multiple', {
+			method: 'POST',
+			body: formData,
+		})
+			.then(res => res.json())
+			.then(json => setFileData(json))
+			.catch(err => {
+				console.log(err.massage)
+			})
+	}
+
+	return (
+		<div className='App'>
+			{Boolean(fileData) && (
+					fileData.data.map((obj, i) => (
+							
+							<img key={i} src={`http://localhost:5000${obj.path}`} />
+					))
+			)}
+			<form>
+				<input type='file' multiple onChange={onSubmitHandler} />
+				<input type='submit' />
+			</form>
+		</div>
+	)
 }
 
-export default App;
+export default App
